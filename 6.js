26551793f78a31076e889120ef62831c0b8adaeb
Kimiya <102611881+Kimiagoudarzi@users.noreply.github.com>
2022-04-17 02:15:53 +0430

    Q6 done

--- 6.js
+++ 6.js
@@ -1,13 +1,5 @@
-function getDate(sp){
-    let now = new Date();
-    let day = now.getDay();
-    let month = now.getMonth() +1;
-    let year = now.getFullYear();
-    
-    if(day < 10) day = "0" + day;
-    
-    if(month < 10 ) month = "0" + month;
-    
-        return (month +sp + day+ sp + year);
-    }
-    console.log(getDate(' / '));
+function sumOfTheRoundedNumber(arr){
+    let result = arr.reduce((a, b) => a+b, 0);
+    return Math.round(result);
+}
+console.log(sumOfTheRoundedNumber( [15.5, 2.3, 1.1, 4.7]));

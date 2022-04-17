84a7601722030fee60978b5a25d8d117a72e37d1
Kimiya <102611881+Kimiagoudarzi@users.noreply.github.com>
2022-04-18 01:13:54 +0430

    Q4 Done

--- 4.js
+++ 4.js
@@ -5,3 +5,15 @@ function remove(array){
     
 }
 console.log(remove([0, 1, 2, 3, 'hi', null , false]));
+
+
+
+// function removeItem(array) { 
+//     let arr = array.filter(function(element){ 
+//     return Boolean(element)===true 
+//     }); 
+//     return arr 
+// } 
+
+// let arr1 =["",1,null,2,false,56,78,0,3,99,NaN,4,5,undefined]; 
+// console.log(removeItem(arr1))

f08f1a40967000d7745ed15c56fcba3d03e98aa9
Kimiya <102611881+Kimiagoudarzi@users.noreply.github.com>
2022-04-18 01:22:12 +0430

    q8 done

---
+++ 8.js
@@ -0,0 +1,6 @@
+let monthName = function(date){ 
+    monthList = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]; 
+      return monthList[date.getMonth()]; 
+    } 
+    console.log(monthName(new Date("10/11/2009"))); 
+    console.log(monthName(new Date("11/13/2014")));

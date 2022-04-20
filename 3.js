389faa88b6d2b325466cb5b5796c5993a1658d7c
Kimiya <102611881+Kimiagoudarzi@users.noreply.github.com>
2022-04-20 16:32:14 +0430

    Q3 Done

--- 3.js
+++ 3.js
@@ -1,13 +1,12 @@
 function upperCase(str){
-
     let arr = str.split(" ");
+
     for (let i = 0; i<arr.length; i++){
         arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1);
     }
+    
     let result = arr.join(" ");
     return result;
 }
-
-
 
 console.log(upperCase('the quick brown fox'));
